9c20a7f6defaab8af09a3be44f28cb882dc13a43
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-05-01 18:46:05 +0300

    style:styled news.html

--- app/main/views.py
+++ app/main/views.py
@@ -2,3 +2,31 @@ from flask import render_template
 from . import main
 from ..requests import get_sources, get_articles
 from ..models_sources import Source
+
+# views
+@main.route('/') # route decorator defining the view function
+def index():
+    """
+   view root page function that returns the index the page and its data
+    """
+    # Getting the popular categories
+    general = get_sources('general')
+    sports = get_sources('sports')
+    business = get_sources('business')
+    technology = get_sources('technology')
+    entertainment = get_sources('entertainment')
+    science = get_sources('science')
+
+
+    title = 'Welcome to NewsPoint Center '
+
+    return render_template('index.html', title=title, general=general, sports=sports, business=business, technology=technology, entertainment=entertainment, science=science) #template name vs view name
+
+@main.route('/<source_id>')
+def articles(source_id):
+    """
+    Articles page
+    """
+    news_source = get_articles(source_id)
+    title = f'Welcome to {source_id}'
+    return render_template('news.html', title=title, name = source_id, news = news_source)
---
+++ start.sh
@@ -0,0 +1,5 @@
+export NEWS_API_KEY='a082fedfb160404da55fe01229c927db'
+export SECRET_KEY='A=QGLZ:EY9VS5a=r/T8VY7c,t'
+
+
+python3.8 manage.py server
